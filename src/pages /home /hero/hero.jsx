1d359268629830a530d1b0513f6f663f13ec11b5
LupeCruz16 <guacruz.16@gmail.com>
2024-10-25 12:27:02 -0700

    Updating router and home page

--- src/pages /home /hero/hero.jsx
+++ src/pages /home /hero/hero.jsx
@@ -1,22 +1,19 @@
 import React from "react";
 import "./hero.css";
-import profileImage from "./assets/profile.jpg"; // Make sure to have an image in this path
+// import profileImage from "./image.png";
 
 function Hero() {
   return (
     <section className="hero">
       <div className="hero-content">
-        <h1>Hello, I'm [Your Name]</h1>
-        <p>
-          A passionate software engineer dedicated to building innovative and
-          user-friendly applications.
-        </p>
+        <h1>Hello, I'm John</h1>
+        <p>A passionate actor dedicated to wrestling.</p>
         <a href="#projects" className="hero-button">
           View My Work
         </a>
       </div>
       <div className="hero-image">
-        <img src={profileImage} alt="Profile" />
+        {/* <img src={profileImage} alt="Profile" /> */}
       </div>
     </section>
   );
